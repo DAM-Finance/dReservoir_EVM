83661f1ffd5e83bd615fea9cf57558b1ebb456b0
1337Hobo <noahfoltz@outlook.com>
2022-08-08 14:42:23 -0400

    To merge

---
+++ makerContracts/Pot.sol
@@ -0,0 +1,156 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+/// Pot.sol -- Dai Savings Rate
+
+pragma solidity ^0.8.9;
+
+/*
+   "Savings Dai" is obtained when Dai is deposited into
+   this contract. Each "Savings Dai" accrues Dai interest
+   at the "Dai Savings Rate".
+
+   This contract does not implement a user tradeable token
+   and is intended to be used with adapters.
+
+         --- `save` your `dai` in the `pot` ---
+
+   - `dsr`: the Dai Savings Rate
+   - `pie`: user balance of Savings Dai
+
+   - `join`: start saving some dai
+   - `exit`: remove some dai
+   - `drip`: perform rate collection
+
+*/
+
+interface VatLike {
+    function move(address,address,uint256) external;
+    function suck(address,address,uint256) external;
+}
+
+contract Pot {
+    // --- Data ---
+    mapping (address => uint256) public wards;
+
+    mapping (address => uint256) public pie;  // Normalised Savings Dai [wad]
+
+    uint256 public Pie;   // Total Normalised Savings Dai  [wad]
+    uint256 public dsr;   // The Dai Savings Rate          [ray]
+    uint256 public chi;   // The Rate Accumulator          [ray]
+
+    address public vow;   // Debt Engine
+    uint256 public rho;   // Time of last drip     [unix epoch time]
+
+    uint256 public live;  // Active Flag
+
+    VatLike public immutable vat;   // CDP Engine
+    uint256 constant RAY = 10 ** 27;
+
+    // --- Events ---
+    event Rely(address indexed usr);
+    event Deny(address indexed usr);
+    event File(bytes32 indexed what, uint256 data);
+    event File(bytes32 indexed what, address data);
+    event Cage();
+    event Drip();
+    event Join(address indexed usr, uint256 wad);
+    event Exit(address indexed usr, uint256 wad);
+
+    modifier auth {
+        require(wards[msg.sender] == 1, "Pot/not-authorized");
+        _;
+    }
+
+    // --- Init ---
+    constructor(address vat_) {
+        wards[msg.sender] = 1;
+        vat = VatLike(vat_);
+        dsr = RAY;
+        chi = RAY;
+        rho = block.timestamp;
+        live = 1;
+        emit Rely(msg.sender);
+    }
+
+    // --- Math ---
+    function _rpow(uint256 x, uint256 n, uint256 base) internal pure returns (uint256 z) {
+        assembly {
+            switch x case 0 {switch n case 0 {z := base} default {z := 0}}
+            default {
+                switch mod(n, 2) case 0 { z := base } default { z := x }
+                let half := div(base, 2)  // for rounding.
+                for { n := div(n, 2) } n { n := div(n,2) } {
+                    let xx := mul(x, x)
+                    if iszero(eq(div(xx, x), x)) { revert(0,0) }
+                    let xxRound := add(xx, half)
+                    if lt(xxRound, xx) { revert(0,0) }
+                    x := div(xxRound, base)
+                    if mod(n,2) {
+                        let zx := mul(z, x)
+                        if and(iszero(iszero(x)), iszero(eq(div(zx, x), z))) { revert(0,0) }
+                        let zxRound := add(zx, half)
+                        if lt(zxRound, zx) { revert(0,0) }
+                        z := div(zxRound, base)
+                    }
+                }
+            }
+        }
+    }
+
+    // --- Administration ---
+    function rely(address usr) external auth {
+        wards[usr] = 1;
+        emit Rely(usr);
+    }
+
+    function deny(address usr) external auth {
+        wards[usr] = 0;
+        emit Deny(usr);
+    }
+
+    function file(bytes32 what, uint256 data) external auth {
+        require(live == 1, "Pot/not-live");
+        require(block.timestamp == rho, "Pot/rho-not-updated");
+        if (what == "dsr") dsr = data;
+        else revert("Pot/file-unrecognized-param");
+        emit File(what, data);
+    }
+
+    function file(bytes32 what, address data) external auth {
+        if (what == "vow") vow = data;
+        else revert("Pot/file-unrecognized-param");
+        emit File(what, data);
+    }
+
+    function cage() external auth {
+        live = 0;
+        dsr = RAY;
+        emit Cage();
+    }
+
+    // --- Savings Rate Accumulation ---
+    function drip() external returns (uint256 tmp) {
+        tmp = _rpow(dsr, block.timestamp - rho, RAY) * chi / RAY;
+        uint256 chi_ = tmp - chi;
+        chi = tmp;
+        rho = block.timestamp;
+        vat.suck(address(vow), address(this), Pie * chi_);
+        emit Drip();
+    }
+
+    // --- Savings Dai Management ---
+    function join(uint256 wad) external {
+        require(block.timestamp == rho, "Pot/rho-not-updated");
+        pie[msg.sender] = pie[msg.sender] + wad;
+        Pie             = Pie             + wad;
+        vat.move(msg.sender, address(this), chi * wad);
+        emit Join(msg.sender, wad);
+    }
+
+    function exit(uint256 wad) external {
+        pie[msg.sender] = pie[msg.sender] - wad;
+        Pie             = Pie             - wad;
+        vat.move(address(this), msg.sender, chi * wad);
+        emit Exit(msg.sender, wad);
+    }
+}
