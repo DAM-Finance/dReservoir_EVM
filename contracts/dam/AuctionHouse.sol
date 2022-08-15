c667ad75fc6b43cae8f3f2bb27e8b879f917a986
Roger Willis <rojw@protonmail.com>
2022-08-15 17:12:53 +0100

    Commited wrong file.

--- contracts/dam/AuctionHouse.sol
+++ contracts/dam/AuctionHouse.sol
@@ -31,27 +31,28 @@ contract AuctionHouse {
     // --- Interfaces and data ---
     //
 
-    // TODO: Rename some of these properties as currently they are confusing given the two stage nature of
-    // the auctions. E.g. "highestBidder" is actually the lowest bidder in the second stage!
-    struct Bid {
+    struct Auction {
         bytes32[]   lotList;            // Collateral types being liquidated
         uint256[]   lotValues;          // collateral amount up for auction             [wad]
-        uint256     lotBid;             // Percentage of collateral in return for bid   [rad]
         uint256     askingAmount;       // Target dPRIME amount                         [rad]
-        uint256     highestBid;         // Highest dPRIME paid                          [rad]
-        address     highestBidder;      // Highest bidder
+
+        uint256     debtBid;            // Highest dPRIME paid                          [rad]
+        uint256     collateralBid;      // Percentage of collateral in return for bid   [ray]
+
         uint256     bidExpiry;          //                                              [unix epoch time]
-        uint256     auctionExpiry;      //                                              [unix epoch time]
+        uint256     auctionExpiry;      //     
+        
+        address     currentWinner;      // Highest bidder                               [unix epoch time]
         address     liquidated;         // Liquidated user
         address     treasury;           // Treasury address
     }
 
-    mapping (uint256 => Bid)    public              auctions;
+    mapping (uint256 => Auction)    public          auctions;
 
     LMCVLike                    public immutable    lmcv;                           // LMCV.
     uint256                     public              live;                           // Active flag
-    uint256                     public              minimumBidIncrease  = 1.05E18;  // 5% minimum bid increase
-    uint256                     public              minimumBidDecrease  = 0.95E27;  // 5% minimum bid decrease
+    uint256                     public              minimumBidIncrease  = 1.05E18;  // 5% minimum debt bid increase
+    uint256                     public              minimumBidDecrease  = 0.95E27;  // 5% minimum collateral bid decrease
     uint256                     public              bidExpiry           = 3 hours;  // 3 hours bid duration         [seconds]
     uint256                     public              auctionExpiry       = 2 days;   // 2 days total auction length  [seconds]
     uint256                     public              auctionId           = 0;        // Monotonic auction ID
@@ -90,9 +91,12 @@ contract AuctionHouse {
         live = 0;
     }
 
-    // TODO: Rename this as this is confusing for the second auction stage.
-    function setMinimumBidIncrease(uint256 rad) external auth {
+    function setMinimumDebtBidIncrease(uint256 rad) external auth {
         minimumBidIncrease = rad;
+    }
+
+    function setMinimumCollateralBidDecrease(uint256 ray) external auth {
+        minimumBidDecrease = ray;
     }
 
     function setBidExpiry(uint256 mins) external auth {
@@ -130,7 +134,7 @@ contract AuctionHouse {
         uint256 askingAmount, 
         bytes32[] calldata lotList, 
         uint256[] calldata lotValues, 
-        uint256 highestBid
+        uint256 debtBid
     ) external auth returns (uint256 id) {
         // Return the current ID and increment.
         id = ++auctionId;
@@ -138,9 +142,9 @@ contract AuctionHouse {
         // Set up struct for this auction.
         auctions[id].lotList        = lotList;
         auctions[id].lotValues      = lotValues;
-        auctions[id].lotBid         = RAY;              // 100% of the collateral.
-        auctions[id].highestBid     = highestBid;
-        auctions[id].highestBidder  = msg.sender;
+        auctions[id].collateralBid  = RAY;
+        auctions[id].debtBid        = debtBid;
+        auctions[id].currentWinner  = msg.sender;
         auctions[id].auctionExpiry  = uint256(block.timestamp) + auctionExpiry;
         auctions[id].liquidated     = user;
         auctions[id].treasury       = treasury;
@@ -162,7 +166,7 @@ contract AuctionHouse {
      */
     function raise(uint256 id, uint256 bid) external {
         // The liquidator contract is always the highest bidder for a new, valid auction.
-        require(auctions[id].highestBidder != address(0), "AuctionHouse/Highest bidder not set");
+        require(auctions[id].currentWinner != address(0), "AuctionHouse/Highest bidder not set");
         // No bids after bid expiry time, which by default is 3 hours after a bid is placed.
         require(auctions[id].bidExpiry > block.timestamp || auctions[id].bidExpiry == 0, "AuctionHouse/Bid expiry reached");
         // Bids can't be placed when the auction has ended.
@@ -170,53 +174,61 @@ contract AuctionHouse {
         // No bids higher than the asking amount.
         require(bid <= auctions[id].askingAmount, "AuctionHouse/Bid higher than asking amount");
         // New bid must be higher than old bid!
-        require(bid > auctions[id].highestBid, "AuctionHouse/Bid must be higher than current highest bid");
+        require(bid > auctions[id].debtBid, "AuctionHouse/Bid must be higher than current highest bid");
         // Bid must increase by the minimum amount or be the asking amount.
-        require(bid * WAD >= minimumBidIncrease * auctions[id].highestBid || bid == auctions[id].askingAmount, "AuctionHouse/Insufficient increase");
+        require(bid * WAD >= minimumBidIncrease * auctions[id].debtBid || bid == auctions[id].askingAmount, "AuctionHouse/Insufficient increase");
         
         // TODO: Add minimum raise functionality.
         
         // Refund the previous highest bidder if there was one and move the increased amount to
-        // the treasury. For the first bid, "highestBid" will be zero, so no DPrime is moved to the
+        // the treasury. For the first bid, "debtBid" will be zero, so no DPrime is moved to the
         // liquidator contract and the whole bid amount is moved from the bidder to the treasury.
-        if (msg.sender != auctions[id].highestBidder) {
-            lmcv.moveDPrime(msg.sender, auctions[id].highestBidder, auctions[id].highestBid);
-            auctions[id].highestBidder = msg.sender;
+        if (msg.sender != auctions[id].currentWinner) {
+            lmcv.moveDPrime(msg.sender, auctions[id].currentWinner, auctions[id].debtBid);
+            auctions[id].currentWinner = msg.sender;
         }
-        lmcv.moveDPrime(msg.sender, auctions[id].treasury, bid - auctions[id].highestBid);
+        lmcv.moveDPrime(msg.sender, auctions[id].treasury, bid - auctions[id].debtBid);
 
-        auctions[id].highestBid = bid;
+        auctions[id].debtBid = bid;
         auctions[id].bidExpiry = uint256(block.timestamp) + bidExpiry;
     }
 
-    function converge(uint256 id, uint256 lotBid) external {
+    /**
+     * In this stage of the auction, participants compete on how much collateral they are willing to accept
+     * for a fixed amount of dPRIME. To do this they submit decreasing bids to the auction. Each bid represents
+     * the percentage of collateral which the auction user is willing to receive in return for the fixed dPRIME 
+     * amount, e.g. a bid of 0.80 means the user is willing to accept 80% of the collatera lup for auction in 
+     * return for the fixed dPRIME price, meaning that 20% of the collateral goes back to the liquidated vault 
+     * user.
+     */
+    function converge(uint256 id, uint256 collateralBid) external {
         // The liquidator contract is always the highest bidder for a new, valid auction.
-        require(auctions[id].highestBidder != address(0), "AuctionHouse/Highest bidder not set");
+        require(auctions[id].currentWinner != address(0), "AuctionHouse/Highest bidder not set");
         // No bids after bid expiry time, which by default is 3 hours after a bid is placed.
         require(auctions[id].bidExpiry > block.timestamp || auctions[id].bidExpiry == 0, "AuctionHouse/Bid expiry reached");
         // Bids can't be placed when the auction has ended.
         require(auctions[id].auctionExpiry > block.timestamp, "AuctionHouse/Auction ended");
         // This stage can only start once stage one has finished.
-        require(auctions[id].highestBid == auctions[id].askingAmount, "AuctionHouse/First phase not finished");
+        require(auctions[id].debtBid == auctions[id].askingAmount, "AuctionHouse/First phase not finished");
         // New lot bids must be lower than the current lot bid.
-        require(lotBid < auctions[id].lotBid, "AuctionHouse/LotBid not lower");
+        require(collateralBid < auctions[id].collateralBid, "AuctionHouse/collateralBid not lower");
         // New lot bids must be lower than the minimum decrease.
-        require(rmul(lotBid, RAY) <= rmul(minimumBidDecrease, auctions[id].lotBid), "AuctionHouse/Insufficient decrease");
+        require(rmul(collateralBid, RAY) <= rmul(minimumBidDecrease, auctions[id].collateralBid), "AuctionHouse/Insufficient decrease");
 
         // The lowest bidder at this stage, if not the highest bidder in the first stage has to move the 
         // amount of dPRIME decided in the first stage to the prior highest bidder.
-        if (msg.sender != auctions[id].highestBidder) {
-            lmcv.moveDPrime(msg.sender, auctions[id].highestBidder, auctions[id].highestBid);
-            auctions[id].highestBidder = msg.sender;
+        if (msg.sender != auctions[id].currentWinner) {
+            lmcv.moveDPrime(msg.sender, auctions[id].currentWinner, auctions[id].debtBid);
+            auctions[id].currentWinner = msg.sender;
         }
 
+        // Return collateral to the user - difference between last collateralBid and current collateralBid.
         for(uint256 i = 0; i < auctions[id].lotList.length; i++) {
-            // Return collateral to the user - difference between last lotBid and current lotBid.
-            uint256 portionToReturn = rmul(auctions[id].lotValues[i], (auctions[id].lotBid - lotBid)); 
+            uint256 portionToReturn = rmul(auctions[id].lotValues[i], (auctions[id].collateralBid - collateralBid)); 
             lmcv.moveCollateral(auctions[id].lotList[i], address(this), auctions[id].liquidated, portionToReturn);
         }
 
-        auctions[id].lotBid = lotBid;
+        auctions[id].collateralBid = collateralBid;
         auctions[id].bidExpiry = uint256(block.timestamp) + bidExpiry;
     }
 
@@ -227,11 +239,15 @@ contract AuctionHouse {
      */
     function end(uint256 id) external {
         // The auction ends after the last bid and auction expiry is reached.
-        require(auctions[id].bidExpiry != 0 && (auctions[id].bidExpiry < block.timestamp || auctions[id].auctionExpiry < block.timestamp), "AuctionHouse/Auction not finished");
+        require(
+            auctions[id].bidExpiry != 0 && (auctions[id].bidExpiry < block.timestamp || auctions[id].auctionExpiry < block.timestamp), 
+            "AuctionHouse/Auction not finished"
+        );
         // The highest bidder gets whatever collateral is left over after stage two of the auction.
         // TODO: Need to multiply lotValues by the % bid for.
         for(uint256 i = 0; i < auctions[id].lotList.length; i++) {
-            lmcv.moveCollateral(auctions[id].lotList[i], address(this), auctions[id].highestBidder, auctions[id].lotValues[i]);
+            uint256 remainingCollateral = rmul(auctions[id].lotValues[i], auctions[id].collateralBid);
+            lmcv.moveCollateral(auctions[id].lotList[i], address(this), auctions[id].currentWinner, remainingCollateral);
         }
         delete auctions[id];
     }
