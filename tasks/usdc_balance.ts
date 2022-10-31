b94fe0e89d500def9912fcf3d257a2f7dcf7a8ac
Roger Willis <rojw@protonmail.com>
2022-10-31 11:41:44 +0000

    Updated scripts to work for localhost and goerli.

--- tasks/usdc_balance.ts
+++ tasks/usdc_balance.ts
@@ -3,17 +3,23 @@ import { task } from "hardhat/config";
 import { ethers } from 'ethers';
 
 task("usdc_balance", "gets the USDC balance for an account")
-  .addParam("user", "The user's address")
-  .setAction(async (taskArgs: any, hre: HardhatRuntimeEnvironment) => {
-	const {deployments} = hre;
-	const {read} = deployments;
+  	.addParam("user", "The user's address")
+  	.setAction(async (taskArgs: any, hre: HardhatRuntimeEnvironment) => {
+		const network = hre.network.name;
+		const {deployments} = hre;
+		const {read} = deployments;
+		let balance;
 
-	const result = await read(
-		"USDC", 
-		{from: taskArgs.user},
-		"balanceOf",
-		taskArgs.user
-	);
-	
-	console.log(`User ${taskArgs.user.substring(0, 10)} USDC balance:`, ethers.utils.formatUnits(result.toString(), 6));
+		switch (network) {
+			case "localhost":
+				balance = await read("USDC", {from: taskArgs.user}, "balanceOf", taskArgs.user);
+				break;
+			case "goerli":
+				balance = await read("USDC", {from: taskArgs.user}, "balanceOf", taskArgs.user);
+				break;
+			default:
+				console.log(`Network ${network} not supported for task usdc_balance.`)
+		}
+		
+		console.log(`User ${taskArgs.user.substring(0, 10)} USDC balance:`, ethers.utils.formatUnits(balance.toString(), 6));
 });
