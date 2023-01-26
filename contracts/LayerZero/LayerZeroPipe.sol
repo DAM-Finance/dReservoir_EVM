be6c222d51a7a8a25fe35a5b41968c1b3deeb0e8
Noah Foltz <nofo@dam.finance>
2023-01-26 13:08:36 -0600

    Initial layer zero teleport fee impl

--- contracts/LayerZero/LayerZeroPipe.sol
+++ contracts/LayerZero/LayerZeroPipe.sol
@@ -12,19 +12,48 @@ interface d2OLike {
     function totalSupply() external view returns (uint256 supply);
     function burn(address,uint256) external;
     function mintAndDelay(address,uint256) external;
+    function mint(address,uint256) external;
 }
 
 contract LayerZeroPipe is OFTCore, IOFT, AuthAdmin("LayerZeroPipe", msg.sender) {
 
-    address public immutable d2OContract;
+    address public immutable    d2OContract;
+    address public              treasury;
+    uint256 public              teleportFee; // [ray]
+
 
     event MintLayerZero(address indexed from, uint256 amount, uint16 _srcChainId);
     event BurnLayerZero(address indexed from, uint256 amount, uint16 _dstChainId);
+    event SetTeleportFee(uint256 teleportFee);
 
     constructor(address _lzEndpoint, address _d2OContract) OFTCore(_lzEndpoint) {
         require(_lzEndpoint != address(0) && _d2OContract != address(0), "d2OConnectorLZ/invalid address");
         d2OContract = _d2OContract;
     }
+
+    //
+    // -- Maths ---
+    //
+    
+    uint256 constant RAY = 10 ** 27;
+    // Can only be used sensibly with the following combination of units:
+    // - `_wadmul(wad, ray) -> wad`
+    function _wadmul(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        z = x * y;
+        require(y == 0 || z / y == x);
+        z = z / RAY;
+    }
+
+    function setTreasury(address _treasury) external auth {
+        require(_treasury != address(0x0), "d2OConnectorLZ/Can't be zero address");
+        treasury = _treasury;
+    }
+
+    function setTeleportFee(uint256 _teleportFee) external auth {
+        require(_teleportFee < RAY, "PSM/Fees must be less than 100%");
+        teleportFee = _teleportFee;
+        emit SetTeleportFee(teleportFee);
+    } 
 
     function supportsInterface(bytes4 interfaceId) public view virtual override(OFTCore, IERC165) returns (bool) {
         return interfaceId == type(IOFT).interfaceId || interfaceId == type(IERC20).interfaceId || super.supportsInterface(interfaceId);
@@ -44,7 +73,9 @@ contract LayerZeroPipe is OFTCore, IOFT, AuthAdmin("LayerZeroPipe", msg.sender) 
     }
 
     function _creditTo(uint16 _srcChainId, address _toAddress, uint _amount) internal virtual override alive {
-        d2OLike(d2OContract).mintAndDelay(_toAddress, _amount);
+        uint256 feeAmount = _wadmul(_amount, teleportFee); // wadmul(wad * ray) = wad
+        d2OLike(d2OContract).mint(treasury, feeAmount);
+        d2OLike(d2OContract).mintAndDelay(_toAddress, _amount - feeAmount);
         emit MintLayerZero(_toAddress, _amount, _srcChainId);
     }
 
---
+++ test/LMCVTests/PipeTesting.js
@@ -0,0 +1,103 @@
+const {expect} = require("chai");
+const {ethers} = require("hardhat");
+
+let owner, addr1, addr2, addr3, addrs;
+let d2oFactory, srcd2o, dstd2o;
+let hyperlanePipeFactory, hyperlanePipe;
+let LZPipeFactory, srcLZPipe, dstLZPipe;
+let lzMockEndpoint, lzMockEndpointFactory;
+let userd2o;
+
+const MAX_INT = "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
+
+
+//Format as wad, ray, rad
+function fwad(wad){ return ethers.utils.parseEther(wad)}
+function fray(ray){ return ethers.utils.parseEther(ray).mul("1000000000")}
+function frad(rad){ return ethers.utils.parseEther(rad).mul("1000000000000000000000000000")}
+//Parse from wad,ray,rad
+function pwad(bigNumber){ return bigNumber.div("1000000000000000000")}
+function pray(bigNumber){ return bigNumber.div("1000000000000000000000000000")} 
+function prad(bigNumber){ return bigNumber.div("1000000000000000000000000000000000000000000000")}
+
+async function setupUser(addr, amounts){
+    let mockTokenConnect = USDCMock.connect(addr);
+    let mockToken2Connect = tokenTwo.connect(addr);
+    
+    await mockTokenConnect.approve(collateralJoin.address);
+    await mockToken2Connect.approve(collatJoinTwo.address, MAX_INT);
+
+    await mockTokenConnect.mint(fwad(amounts.at(0)));
+    await mockToken2Connect.mint(fwad(amounts.at(0)));
+}
+
+describe("Testing LMCV", function () {
+
+    before(async function () {
+        d2oFactory                  = await ethers.getContractFactory("d2O");
+        hyperlanePipeFactory        = await ethers.getContractFactory("HyperlanePipe");
+        LZPipeFactory               = await ethers.getContractFactory("LayerZeroPipe");
+        lzMockEndpointFactory       = await ethers.getContractFactory("LZEndpointMock");
+    });
+
+    beforeEach(async function () {
+        [owner, addr1, addr2, addr3, ...addrs] = await ethers.getSigners();
+
+        srcd2o          = await d2oFactory.deploy();
+        srcLZEndpoint   = await lzMockEndpointFactory.deploy("1");
+        srcLZPipe       = await LZPipeFactory.deploy(srcLZEndpoint.address, srcd2o.address);
+        
+        dstd2o          = await d2oFactory.deploy();
+        dstLZEndpoint   = await lzMockEndpointFactory.deploy("2");
+        dstLZPipe       = await LZPipeFactory.deploy(dstLZEndpoint.address, dstd2o.address);
+
+        await srcd2o.rely(srcLZPipe.address);
+        await dstd2o.rely(dstLZPipe.address);
+
+        await srcLZPipe.setTreasury(owner.address);
+        await srcLZPipe.setTeleportFee(fray("0.003"));
+        await srcLZPipe.setTrustedRemoteAddressAuth("2", dstLZPipe.address);
+        await srcLZEndpoint.setDestLzEndpoint(dstLZPipe.address, dstLZEndpoint.address);
+        
+
+        await dstLZPipe.setTreasury(owner.address);
+        await dstLZPipe.setTeleportFee(fray("0.003"));
+        await dstLZPipe.setTrustedRemoteAddressAuth("1", srcLZPipe.address);
+        await dstLZEndpoint.setDestLzEndpoint(srcLZPipe.address, srcLZEndpoint.address);
+
+        userd2o = srcd2o.connect(addr1);
+
+
+
+    });
+
+    describe("Layer Zero Pipe testing", function () {
+
+        it("Should properly transfer using mock endpoint with fees", async function () {
+            console.log("Treasury:" + await srcLZPipe.treasury());
+            console.log("Teleport Fee:" + await srcLZPipe.teleportFee());
+
+            let userSrcLZPipe = srcLZPipe.connect(addr1);
+
+            await srcd2o.mint(addr1.address, fwad("10000"));
+
+            let adapterParams = ethers.utils.solidityPack(
+                ['uint16','uint256'],
+                [1, 200000]
+            )
+
+            let feeEstimate = await userSrcLZPipe.estimateSendFee("2", addr1.address, fwad("1000"), false, adapterParams);
+            await userSrcLZPipe.sendFrom(addr1.address, "2", addr1.address, fwad("1000"), addr1.address, addr1.address, [], {value: feeEstimate.nativeFee});
+
+            expect(await dstd2o.balanceOf(addr1.address)).to.equal(fwad("997"));
+            expect(await dstd2o.balanceOf(owner.address)).to.equal(fwad("3"));
+
+            expect(await srcd2o.balanceOf(addr1.address)).to.equal(fwad("9000"));
+            expect(await srcd2o.balanceOf(owner.address)).to.equal(fwad("0"));
+        });
+    });
+
+    
+
+});
+
