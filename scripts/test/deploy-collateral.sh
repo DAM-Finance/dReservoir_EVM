fafb8cdd749f3a3f6c1ccc8dd64cfba89912b0a2
Roger Willis <rojw@protonmail.com>
2023-02-10 12:45:43 +0000

    Added easy to use scripts for deploying and configuring testnet contracts.

---
+++ scripts/test/deploy-collateral.sh
@@ -0,0 +1,4 @@
+export TOKEN_SYMBOL="USDC"
+export TOKEN_NAME="Test USDC"
+
+npx hardhat deploy --network $1 --tags collateral
---
+++ scripts/test/deploy-psm.sh
@@ -0,0 +1,4 @@
+export TOKEN_SYMBOL="USDC"
+export TREASURY_ADDRESS="0x70997970C51812dc3A010C7d01b50e0d17dc79C8"
+
+npx hardhat deploy --network $1 --tags psm
