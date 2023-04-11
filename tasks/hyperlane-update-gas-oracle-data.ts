e8940265d83df7ba92457b785fe7528d16e53fd7
Roger Willis <rojw@protonmail.com>
2023-04-11 12:54:09 +0100

    Set up update gas oracle script.

--- tasks/hyperlane-update-gas-oracle-data.ts
+++ tasks/hyperlane-update-gas-oracle-data.ts
@@ -1,21 +1,30 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { task } from "hardhat/config";
+import { ethers } from 'ethers';
 
-task("hyperlane-update-gas-oracle-data", "updates the prioce data for a specified gas oracle")
+task("hyperlane-set-gas-oracle-address", "sets the gas oracle address for a specific remote")
   .addParam("remoteId", "The remote desintation id")
-  .addParam("address", "The gas oracle address")
+  .addParam("exchangeRate", "The network to network exchange rate")
+  .addParam("gasPrice", "The current gas price on the remote network")
   .setAction(async (taskArgs: any, hre: HardhatRuntimeEnvironment) => {
     const {deployments, getNamedAccounts} = hre;
 	  const {execute} = deployments;
     const {deployer} = await getNamedAccounts();
 
     const remoteId = taskArgs.remoteId;
-    const address = taskArgs.address;
+    const exchangeRate = taskArgs.exchangeRate;
+    const gasPrice = taskArgs.gasPrice;
+
+  //   struct RemoteGasDataConfig {
+  //     uint32 remoteDomain;
+  //     uint128 tokenExchangeRate;
+  //     uint128 gasPrice;
+  //   }
 
     await execute(
-      "InterchainGasPaymaster",
+      "StorageGasOracle",
       {from: deployer, log: true},
-      "setGasOracles",
-      [{remoteId, address}]
+      "setRemoteGasData",
+      [{remoteDomain: remoteId, tokenExchangeRate: exchangeRate, gasPrice: gasPrice}]  // May need to format this as a 2 dimensional array. Tuple is '[...]' in etherum's ABI spec.
   );
 });
